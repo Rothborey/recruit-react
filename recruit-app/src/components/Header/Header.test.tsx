a024247b1695729499949be2e747b2b8d2c5c050
Rothborey <37228780+Rothborey@users.noreply.github.com>
2019-12-15 21:21:25 +1100

    chore: Ignore test warnings

--- recruit-app/src/components/Header/Header.test.tsx
+++ recruit-app/src/components/Header/Header.test.tsx
@@ -4,6 +4,22 @@ import "@testing-library/jest-dom/extend-expect";
 import { Header } from "./Header";
 import { GlobalStateProvider } from "../../GlobalState";
 
+// https://github.com/testing-library/react-testing-library#suppressing-unnecessary-warnings-on-react-dom-168
+// suppress unwanted warnings
+const originalError = console.error;
+beforeAll(() => {
+  console.error = (...args: string[]) => {
+    if (/Warning.*react-modal/.test(args[0])) {
+      return;
+    }
+    originalError.call(console, ...args);
+  };
+});
+
+afterAll(() => {
+  console.error = originalError;
+});
+
 test("should show menu content when menu button is clicked", () => {
   const { getByTitle, getByText } = render(
     <GlobalStateProvider>
@@ -49,7 +65,7 @@ test("should show Menu title when menu button clicked", () => {
   expect(menuTitle).toBeInTheDocument();
 });
 
-test("should show burger menu icon when menu is closed clicked", () => {
+test("should show burger menu icon when menu is closed", () => {
   const { getByTitle, getByText } = render(
     <GlobalStateProvider>
       <Header />
